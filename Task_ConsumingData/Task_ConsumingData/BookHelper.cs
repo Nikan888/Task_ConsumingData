f49f0b60fecd3aef3e6b06f2306d58617345529c
Nikan <32056409+Nikan888@users.noreply.github.com>
2018-05-13 22:10:41 +0300

    Partial

---
+++ Task_ConsumingData/Task_ConsumingData/BookHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_ConsumingData
+{
+    public class BookHelper
+    {
+        List<Book> books;
+
+        public BookHelper()
+        {
+            books = new List<Book>();
+        }
+
+        public void AddBookToList(string name, string authorName, string genre, double price)
+        {
+            books.Add(new Book
+            {
+                Name = name,
+                AuthorName = authorName,
+                Genre = genre,
+                Price = price
+            });
+        }
+
+        public void RemoveBookFromList(Book book)
+        {
+            books.Remove(book);
+        }
+    }
+}
--- Task_ConsumingData/Task_ConsumingData/Program.cs
+++ Task_ConsumingData/Task_ConsumingData/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
 
 namespace Task_ConsumingData
 {
@@ -10,6 +13,80 @@ namespace Task_ConsumingData
     {
         static void Main(string[] args)
         {
+            BookHelper bookHelper = new BookHelper();
+            bookHelper.AddBookToList("1984", "Orwell", "Social Science Fiction", 25);
+            bookHelper.AddBookToList("Dune", "Herbert", "Science Fiction", 74);
+            bookHelper.AddBookToList("Starship Troopers", "Heinlein", "Military Science Fiction", 98);
+            bookHelper.AddBookToList("Fahrenheit 451", "Bradbury", "Dystopian", 12);
+            bookHelper.AddBookToList("Nightfall", "Asimov", "Science Fiction", 115);
+            bookHelper.AddBookToList("Hard to Be a God", "Strugatsky", "Science Fiction", 24);
+
+            WriteToXmlFile<List<Book>>(AppDomain.CurrentDomain.BaseDirectory, bookHelper);
+            WriteToJsonFile<List<Book>>(AppDomain.CurrentDomain.BaseDirectory, bookHelper);
+        }
+
+        public static void WriteToXmlFile<T>(string filePath, T objectToWrite, bool append = false) where T : new()
+        {
+            TextWriter writer = null;
+            try
+            {
+                var serializer = new XmlSerializer(typeof(T));
+                writer = new StreamWriter(filePath, append);
+                serializer.Serialize(writer, objectToWrite);
+            }
+            finally
+            {
+                if (writer != null)
+                    writer.Close();
+            }
+        }
+
+        public static T ReadFromXmlFile<T>(string filePath) where T : new()
+        {
+            TextReader reader = null;
+            try
+            {
+                var serializer = new XmlSerializer(typeof(T));
+                reader = new StreamReader(filePath);
+                return (T)serializer.Deserialize(reader);
+            }
+            finally
+            {
+                if (reader != null)
+                    reader.Close();
+            }
+        }
+
+        public static void WriteToJsonFile<T>(string filePath, T objectToWrite, bool append = false) where T : new()
+        {
+            TextWriter writer = null;
+            try
+            {
+                var contentsToWriteToFile = Newtonsoft.Json.JsonConvert.SerializeObject(objectToWrite);
+                writer = new StreamWriter(filePath, append);
+                writer.Write(contentsToWriteToFile);
+            }
+            finally
+            {
+                if (writer != null)
+                    writer.Close();
+            }
+        }
+
+        public static T ReadFromJsonFile<T>(string filePath) where T : new()
+        {
+            TextReader reader = null;
+            try
+            {
+                reader = new StreamReader(filePath);
+                var fileContents = reader.ReadToEnd();
+                return Newtonsoft.Json.JsonConvert.DeserializeObject<T>(fileContents);
+            }
+            finally
+            {
+                if (reader != null)
+                    reader.Close();
+            }
         }
     }
 }
